dff4906494767c2966e8dd892082cdfef133696b
Patrik Keller <patrik@keller-re.de>
2017-09-01 14:29:55 +0200

    imporved bot with reply button

--- bot/bot.py
+++ bot/bot.py
@@ -1,29 +1,49 @@
-from telegram.ext import Updater, CommandHandler
+#!/usr/bin/env python3
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+import telegram
+
+import logging
 
 import subprocess
 import os
 
 def start(bot, update):
-    update.message.reply_text('Welcome')
+    send_portfolio(bot, update.message.from_user.id)
 
-def portfolio(bot, update):
+def callback(bot, update):
+    query = update.callback_query
+    try:
+        user_id = int(query.data)
+        send_portfolio(bot, user_id)
+    except:
+        logging.warning('Invalid callback data in update "{}"'.format(update))
+    query.edit_message_reply_markup()
+
+def error(bot, update, error):
+    logging.warning('Update "%s" caused error "%s"' % (update, error))
+
+def send_portfolio(bot, user_id):
     # run project main on <user_id>.json
-    filename = '{}.json'.format(update.message.from_user.id)
+    filename = '{}.json'.format(user_id)
     if os.path.isfile(filename):
         ret = subprocess.check_output(
             [ '../main.native',
-                '{}.json'.format(update.message.from_user.id) ]
+                '{}.json'.format(user_id) ]
             , universal_newlines = True )
         ret = "```\n{:s}\n```".format(ret)
+        btn = telegram.InlineKeyboardButton('Refresh', callback_data='{}'.format(user_id))
+        kb  = telegram.InlineKeyboardMarkup([[btn]])
+        bot.send_message(user_id, text=ret, parse_mode='markdown', reply_markup=kb)
     else:
-        ret = 'You are not registered!'
-    bot.send_message(update.message.chat_id, text=ret, parse_mode='markdown')
-    #  parse_mode=telegram.ParseMode.MARKDOWN
-    #  update.message.reply_text(f.read())
+        ret = 'You are not registrated. This bot will not help you.'
+        bot.send_message(user_id, text=ret, parse_mode='markdown')
+
+
 updater = Updater('437620095:AAE8Gtpxu8D8W_nJZ238I0Q_mBOm6WylFVk')
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('do', portfolio))
+updater.dispatcher.add_handler(CallbackQueryHandler(callback))
+updater.dispatcher.add_error_handler(error)
 
 # Poll once per second should be enough
 updater.start_polling(poll_interval=1)

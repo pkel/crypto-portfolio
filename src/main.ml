bcfc7e58ea5b00d4a05896b89623b2b3f7f9e06e
Patrik Keller <patrik@keller-re.de>
2017-11-18 21:39:41 +0100

    Ordering via command line

--- src/main.ml
+++ src/main.ml
@@ -11,7 +11,7 @@ let extend_cmc amount entry =
   { o = entry ; amount; value = amount *. entry.price }
 
 type report_data =
-  { entries : entry_ext list
+  { assets : entry_ext list
   (* sum in fiat *)
   ; sum : float
   (* changes in percent *)
@@ -20,26 +20,45 @@ type report_data =
   ; change_7d : float
   }
 
-let prepare_report cmc cfg =
+let prepare_report cmc assets =
   let open PortfolioSpec_t in
-  let entries = List.map ~f:(fun spec ->
+  let assets = List.map ~f:(fun spec ->
     Map.find cmc spec.id
     |> function
       | Some x -> extend_cmc spec.amount x
       | None -> Printf.eprintf "Invalid id %s in portfolio specification\n"
             spec.id ; exit 1
-    ) cfg.assets
-    |> List.sort ~cmp:(fun a b -> Pervasives.compare b.value a.value)
+    ) assets
   in
   let sum, d1h, d24h, d7d = List.fold ~f:( fun (sum, d1h, d24h, d7d) e ->
       ( sum  +.  e.value
       , d1h  +. (e.value /. (1. +. e.o.percent_change_1h  /. 100.))
       , d24h +. (e.value /. (1. +. e.o.percent_change_24h /. 100.))
       , d7d  +. (e.value /. (1. +. e.o.percent_change_7d  /. 100.))
-      ) ) ~init:(0.,0.,0.,0.) entries in
+      ) ) ~init:(0.,0.,0.,0.) assets in
   let change d = (sum -. d) /. d *. 100. in
-  { entries ; sum ; change_1h = change d1h ; change_24h = change d24h
+  { assets ; sum ; change_1h = change d1h ; change_24h = change d24h
   ; change_7d = change d7d }
+
+let allowed_orders =
+  "amount, rate or value"
+
+let reorder desc = function
+  | None -> (fun x -> x)
+  | Some o ->
+      let open CoinMarketCap in
+      let get = match o with
+        | "rate" ->   (fun a -> a.o.price)
+        | "amount" -> (fun a -> a.amount)
+        | "value" ->  (fun a -> a.value)
+        | _ -> Printf.eprintf
+                  "Can't order by %s. Please specify one out of %s.\n"
+                  o allowed_orders; exit 1
+      in let cmp = fun a b ->
+        Pervasives.compare (get a) (get b) * (if desc then -1 else 1)
+      in fun rep ->
+        let assets = List.sort ~cmp rep.assets
+        in { rep with assets }
 
 
 module Short = struct
@@ -56,7 +75,7 @@ module Short = struct
   let print d =
     printf   "Assets:\n";
     printf "      24h    7d     rate\n";
-    List.iter ~f:print_entry d.entries ;
+    List.iter ~f:print_entry d.assets ;
     printf "\nCombined:\n";
     printf "      24h    7d    value\n";
     printf "    %+5.1f%+6.1f %.2e\n"
@@ -80,20 +99,23 @@ module Long = struct
   let print d =
     printf   "Assets:\n";
     printf "        1h   24h     7d      rate    amount     value\n";
-    List.iter ~f:print_entry d.entries ;
+    List.iter ~f:print_entry d.assets ;
     printf "\nCombined:\n";
     printf "        1h   24h     7d                         value\n";
     printf "    %+6.1f%+6.1f%+7.1f                      %.2e\n"
       d.change_1h d.change_24h d.change_7d d.sum
 end
 
-let exec short config () =
+let exec desc order short config () =
   let cfg = PortfolioSpec.read config in
+  let reorder = reorder desc order in
   let data =
     CoinMarketCap.(get ~quote:EUR ())
   in
   let print = if short then Short.print else Long.print in
-  prepare_report data cfg |> print
+  prepare_report data cfg.assets
+  |> reorder
+  |> print
 
 let readme () =
   String.concat ~sep:"\n"
@@ -104,6 +126,8 @@ let readme () =
 let spec =
   let open Command.Spec in
   empty
+  +> flag ~aliases:["desc"]  "d" no_arg ~doc:"sort descending instead of ascending"
+  +> flag ~aliases:["order"] "o" (optional string) ~doc:("order sort assets by " ^ allowed_orders )
   +> flag ~aliases:["short"] "s" no_arg ~doc:"print short version of the report"
   +> anon (maybe_with_default "portfolio.json" ("configuration" %: string))
 
